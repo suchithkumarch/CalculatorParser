e5360305e9a01a1da7f5d470cb67eafd55806d43
suchithkumarch <44491926+suchithkumarch@users.noreply.github.com>
2022-01-19 12:57:10 +0530

    Update Main.scala

--- Main.scala
+++ Main.scala
@@ -4,9 +4,9 @@ object Main extends App {
   print(">>")
   val input = scala.io.StdIn.readLine()
   val sample = new Lexer(input)
-  val l = sample.tokenize("", List(), input, 0)
-  val p = new Parser(sample)
-  val res = p.solve()
-  println(res)
+  val tokenList = sample.tokenize("", List(), input, 0)
+  val sampleParser = new Parser(sample)
+  val result = sampleParser.solve()
+  println(result)
 }
 
